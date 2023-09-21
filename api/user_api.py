82aeb94c11ef81128eda98f70f80a51cb7e93924
Chen Yu Ling <yo0.guitar27@ChendeMac-mini.local>
2023-09-22 05:46:55 +0800

    finish member system ,use Jinja2 manage HTML

--- api/user_api.py
+++ api/user_api.py
@@ -1,6 +1,8 @@
 from flask import *
 from mysql.connector import pooling
-import json,jwt,datetime,pytz
+import json
+import jwt
+import datetime
 
 db_config = {
     "host": "localhost",
@@ -17,6 +19,38 @@ connection_pool = pooling.MySQLConnectionPool(
 
 user_info = Blueprint("user_api", __name__)
 
+SECRET_KEY = "Yo0-secret-key"
+
+def execute_query(query,params=None):
+    connection = connection_pool.get_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SET GLOBAL group_concat_max_len = 102400;")
+	
+    cursor.execute(query,params)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
+def handle_error(message):
+	return json_process_utf8({"error": True, "message": message}), 500
+
+def json_process_utf8(result):
+	result = Response(json.dumps(result, ensure_ascii = False),content_type ='application/json;charset=utf-8')
+	return result
+
+def generate_jwt_token(payload):
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
+
+def verify_jwt_token(token):
+    try:
+        decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return decoded_payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
 
 @user_info.route("/api/user", methods=["POST"])
 def register_user():
@@ -41,93 +75,53 @@ def register_user():
     
     except Exception as error:
         return handle_error("伺服器內部錯誤")
-    
+      
+@user_info.route("/api/user/auth", methods=["PUT"])
+def auth_user():
+    try:
+        auth_data = request.get_json()   
+        search_member_query = """
+        SELECT id, name, email
+        FROM member
+        WHERE email = %s
+        AND password = %s
+        """
+        search_result = execute_query(search_member_query,(auth_data["email"], auth_data["password"]))
+        if search_result:
+            user_info = search_result[0]  
+            payload = {
+                "user_id": user_info["id"],
+                "user_name": user_info["name"],
+                "user_email": user_info["email"],
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+            }
+            token = generate_jwt_token(payload)
+            return jsonify({"token": token}), 200
+        else:
+            return json_process_utf8({"error": True, "message": "登入失敗，帳號或密碼錯誤或其他原因"}), 400
+    except Exception as error:
+        return handle_error("伺服器內部錯誤")
+        
 @user_info.route("/api/user/auth", methods=["GET"])
 def get_user():
-    login_info = request.get_data()
-    check_user_login(login_info)  
-    # if member_info:
-    #     # 如果会员已登录，返回会员信息
-    #     response_data = {
-    #         "data": member_info
-    #     }
-    #     return jsonify(response_data)
-    # else:
-    #     # 如果会员未登录，返回null
-    #     return jsonify({"data": None})
+    print("Getting")
+    auth_header = request.headers.get("Authorization")
+    
+    if not auth_header:
+        return json_process_utf8({"data": None})
+    
+    token = auth_header.replace("Bearer ", "")
+    decoded_payload = verify_jwt_token(token)
 
-
-def check_user_login(login_info):
-    print(login_info)
-
-
-def execute_query(query,params=None):
-    connection = connection_pool.get_connection()
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SET GLOBAL group_concat_max_len = 102400;")
-	
-    cursor.execute(query,params)
-    result = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return result
-
-def handle_error(message):
-	return json_process_utf8({"error": True, "message": message}), 500
-
-def json_process_utf8(result):
-	result = Response(json.dumps(result, ensure_ascii = False),content_type ='application/json;charset=utf-8')
-	return result
-
-
-
-
-# @user_info.route("/api/generate_token")
-# def generate_token():
-#     taipei_timezone = pytz.timezone('Asia/Taipei')
-#     expiration_time = datetime.datetime.utcnow().now(taipei_timezone) + datetime.timedelta(minutes=1)
-
-#     # 構建payload
-#     payload = {
-#         "user_id": 123,
-#         "username": "example_user",
-#         "exp": expiration_time # 使用時間戳表示過期時間
-#     }
-#     # 密鑰，這應該與生成 Token 時使用的密鑰一致
-#     secret_key = "Yo0_secret_key"
-
-#     # 生成JWT Token
-#     token = jwt.encode(payload, secret_key, algorithm="HS256")
-
-#     print(expiration_time)
-#     return jsonify({"token": token}), 200
-
-
-# @user_info.route("/api/validate_token", methods=["POST"])
-# def validate_token():
-#     token_to_decode = request.json.get("token")
-
-#     print(token_to_decode)
-#     secret_key = "Yo0_secret_key"
-
-#     try:
-#         decoded_payload = jwt.decode(token_to_decode, secret_key, algorithms=["HS256"])
-#         print(decoded_payload)
-#         return json_process_utf8({"message": "Token 驗證成功", "payload": decoded_payload}), 200
-#     except jwt.ExpiredSignatureError:
-#         return json_process_utf8({"error": True, "message": "Token 已過期"}), 401
-#     except jwt.InvalidTokenError:
-#         return jsonify({"error": True, "message": "無效的 Token"}), 401
-
-
-
-
-
-# log_in = True
-# @user_info.route("/api/user/auth")
-# def get_current_user():
-#      result = {"data": {"id": 1, "name": "彭彭彭", "email": "ply@ply.com"}}
-#      if log_in:
-#           return json_process_utf8(result)
-     
-
+    
+    if decoded_payload is None:
+        return  json_process_utf8({"data":None})
+    
+    login_info = {
+        "data": {
+            "id": decoded_payload["user_id"],
+            "name": decoded_payload["user_name"],
+            "email": decoded_payload["user_email"]
+        }
+    }
+    return json_process_utf8(login_info), 200
--- static/member.js
+++ static/member.js
@@ -1,5 +1,6 @@
 const signinBackground = document.querySelector(".signin-background");
-const loginButton = document.getElementById("signin-screen");
+const memberButton = document.getElementById("signin-screen");
+const userWelcome = document.getElementById("user-welcome");
 
 const signinContainer = document.querySelector(".signin-main-container");
 const exitButton1 = document.querySelector(".exit-img1");
@@ -7,6 +8,7 @@ const signinEmail = document.getElementById("signin-email");
 const signinPassword = document.getElementById("signin-password");
 const signinButton = document.querySelector(".signin-button");
 const signupSwitchButton = document.querySelector(".switch-signup-mode");
+const alertContainer1 = document.getElementById("alert-container1");
 
 const signupContainer = document.querySelector(".signup-main-container");
 const exitButton2 = document.querySelector(".exit-img2");
@@ -15,14 +17,7 @@ const signupEmail = document.getElementById("signup-email");
 const signupPassword = document.getElementById("signup-password");
 const signupButton = document.getElementById("register-button");
 const signinSwitchButton = document.querySelector(".switch-signin-mode");
-const alertContainer = document.getElementById("alert-container");
-
-loginButton.addEventListener("click", () => {
-  signinBackground.style.display = "flex";
-  setTimeout(() => {
-    signinContainer.style.top = "80px";
-  }, 1);
-});
+const alertContainer2 = document.getElementById("alert-container2");
 
 exitButton1.addEventListener("click", () => {
   signinContainer.style.top = "-400px";
@@ -69,13 +64,11 @@ signupButton.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       const errorMessage = data.message;
-      const alertParagraph = document.createElement("p");
-      alertContainer.appendChild(alertParagraph);
 
       if (data.error) {
-        alertParagraph.textContent = errorMessage;
+        alertContainer2.textContent = errorMessage;
       } else {
-        alertContainer.textContent = "註冊成功";
+        alertContainer2.textContent = "註冊成功";
         signupName.value = "";
         signupEmail.value = "";
         signupPassword.value = "";
@@ -85,7 +78,7 @@ signupButton.addEventListener("click", () => {
         }, 1000);
         setTimeout(() => {
           signinContainer.style.top = "80px";
-          alertContainer.innerHTML = "";
+          alertContainer2.innerHTML = "";
         }, 1400);
       }
     })
@@ -94,36 +87,88 @@ signupButton.addEventListener("click", () => {
     });
 });
 
-
 signinButton.addEventListener("click", () => {
   const signinData = {
     email: signinEmail.value,
     password: signinPassword.value,
   };
-  console.log(signinData);
 
   fetch("/api/user/auth", {
-    method: "GET",
+    method: "PUT",
     headers: {
-      Accept: "application/json",
-      },
-    body: JSON.stringify(signinData)
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(signinData),
   })
     .then((response) => response.json())
     .then((data) => {
-    //   // 在这里处理返回的会员信息
-    //   if (data && data.data) {
-    //     const memberData = data.data;
-    //     // 在这里使用会员信息，例如显示会员的姓名和电子邮件
-    //     console.log("会员ID:", memberData.id);
-    //     console.log("会员姓名:", memberData.name);
-    //     console.log("会员电子邮件:", memberData.email);
-    //   } else {
-    //     // 如果data为null，则表示未登录
-    //     console.log("未登录");
-    //   }
+      const errorMessage = data.message;
+
+      if (data.error) {
+        alertContainer1.textContent = errorMessage;
+        signinEmail.value = "";
+        signinPassword.value = "";
+      } else {
+        alertContainer1.textContent = "登入成功";
+
+        localStorage.setItem("token", data.token);
+
+        signinEmail.value = "";
+        signinPassword.value = "";
+
+        location.reload();
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+});
+
+let getCurrentUser = () => {
+  const token = localStorage.getItem("token");
+
+  fetch("/api/user/auth", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("未登錄或無效的令牌");
+      }
+    })
+    .then((data) => {
+      if (data.data === null) {
+        userWelcome.style.display = "none";
+        memberButton.textContent = "登入/註冊";
+        memberButton.addEventListener("click", () => {
+          signinBackground.style.display = "flex";
+          setTimeout(() => {
+            signinContainer.style.top = "80px";
+          }, 1);
+        });
+      } else {
+        userWelcome.style.display = "block";
+        userWelcome.textContent = `${data.data["name"]}，歡迎光臨`;
+        memberButton.textContent = "登出系統";
+
+        memberButton.addEventListener("click", () => {
+          localStorage.removeItem("token");
+          location.reload();
+        });
+      }
+
+      console.log(data.data);
+    })
+    .catch((error) => {
+      console.error("發生錯誤:", error);
+    });
+};
+
+window.addEventListener("load", () => {
+  getCurrentUser();
 });
